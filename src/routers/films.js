9c96afc3dbbe9bd036a81b248f6b4bc257e2a68e
werikaapelfeler <werika.aps@gmail.com>
2021-10-05 15:32:04 -0300

    Update films.js

--- src/routers/films.js
+++ src/routers/films.js
@@ -7,10 +7,6 @@ const router = express.Router()
 
 //router.use(authMiddleware)
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, users: req.userId })
-})
-
 router.post('/create', async (req, res) => {
     try {
         let { title, synopsis, enabled, actors } = req.body
@@ -31,6 +27,37 @@ router.post('/create', async (req, res) => {
         return res.status(400).send({ error: "Error creating film" })
     }
 
+})
+
+router.put('/update/:id', async (req, res) => {
+    try {
+        let { title, synopsis, enabled, actors } = req.body
+
+        const film = await Film.findByIdAndUpdate(req.params.id, { title, synopsis, enabled }, { new: true })
+        await film.save()
+
+        /*actors.map(actor => {
+            const filmActor = new Actor({ ...actor, film: film._id })
+            filmActor.save().then(actor => {
+            film.actors.push(actor)
+            })
+        })*/
+        res.status(200).send({ film: film, msg: "Successfully updating" })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(400).send({ error: "Error updating film" })
+    }
+
+})
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        await Film.findByIdAndUpdate(req.params.id, { enabled: false }, { new: true })
+        res.status(200).send({ msg: "Successfully deleted" })
+    } catch (error) {
+        res.status(400).send({ error: "Error deleting" })
+    }
 })
 
 module.exports = app => app.use('/films', router)
