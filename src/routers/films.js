93b58d80c27b67fc4ef6b41058bd9a23b55750d4
werikaapelfeler <werika.aps@gmail.com>
2021-10-05 15:27:16 -0300

    Update films.js

--- src/routers/films.js
+++ src/routers/films.js
@@ -1,22 +1,36 @@
 const express = require('express')
-const authMiddleware = require ('../middlewares/auth')
+const authMiddleware = require('../middlewares/auth')
+const Actor = require('../models/Actor')
+const Film = require('../models/Film')
 
 const router = express.Router()
 
-router.use(authMiddleware)
+//router.use(authMiddleware)
 
 router.get('/', (req, res) => {
-    res.send({ ok: true, users: req.userId})
+    res.send({ ok: true, users: req.userId })
 })
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     try {
-        let {title, synopsis, enabled, filmActor } = req.body
-        
-    } catch {
-        return res.status(400).send({error: "Error creating film"})
+        let { title, synopsis, enabled, actors } = req.body
+
+        const film = await Film.create({ title, synopsis, enabled })
+
+        /*actors.map(actor => {
+            const filmActor = new Actor({ ...actor, film: film._id })
+            filmActor.save().then(actor => {
+                film.actors.push(actor)
+            })
+        })*/
+
+        return res.send({ film })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(400).send({ error: "Error creating film" })
     }
-    
+
 })
 
 module.exports = app => app.use('/films', router)
