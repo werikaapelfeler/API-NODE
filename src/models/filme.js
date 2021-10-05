45ebe92fc39352330167c300f1e136b08af9ee22
werikaapelfeler <werika.aps@gmail.com>
2021-10-05 12:44:02 -0300

    Model Film

---
+++ src/models/filme.js
@@ -0,0 +1,25 @@
+const mongoose = require('../database')
+
+const FilmSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    synopsis: {
+        type: String,
+        required: true,
+    },
+    enabled: {
+        type: Boolean,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+const Film = mongoose.model('film', FilmSchema)
+
+
+module.exports = Film;
