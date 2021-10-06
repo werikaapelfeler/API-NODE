8e23826c282768d7d12e8b4315374dabec2332ab
werikaapelfeler <werika.aps@gmail.com>
2021-10-06 13:27:51 -0300

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -32,7 +32,7 @@ router.post("/register-admin", async (req, res) => {
     await userRegister(req.body, "admin", res);
 });
 
-router.post('/autentication', async (req, res) => {
+router.post('/login', async (req, res) => {
     const { email, password } = req.body
 
     const user = await User.findOne({ email }).select('+password')
